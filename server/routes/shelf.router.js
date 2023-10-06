110888be7effc8cebf9270051de0f813d6ebda1d
NodakAttack <125848776+NodakAttack@users.noreply.github.com>
2023-10-05 20:55:56 -0500

    base mode complete, stretch 2/3 done

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,32 +1,84 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * Get all of the items on the shelf
  */
-router.get('/', (req, res) => {
-  let queryText = `SELECT * FROM "item"`;
-  pool.query(queryText).then((result) => {
-    res.send(result.rows);
-  }).catch((error) => {
-    console.log(error);
-    res.sendStatus(500);
-  });
+router.get("/", (req, res) => {
+  console.log("/pet GET route");
+  console.log("is authenticated?", req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    let queryText = `SELECT item.*, "user".username
+    FROM "item"
+    JOIN "user" ON "user".id = item.user_id;`;
+    pool
+      .query(queryText)
+      .then((result) => {
+        res.send(result.rows);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
-  // endpoint functionality
+router.post("/", (req, res) => {
+  console.log("/pet POST route");
+  console.log(req.body);
+
+  // req.isAuthenticated() is a function provided by passport. it returns either true or false
+  console.log("is authenticated?", req.isAuthenticated());
+  if (req.isAuthenticated()) {
+    console.log("user", req.user);
+    // add the pet to our database
+    let queryText = `INSERT INTO "item" ("description", "image_url", "user_id")
+                    VALUES ($1, $2, $3);`;
+
+    // ! req.user.id is the currently logged in user id
+    // always use req.user.id for current user
+    pool
+      .query(queryText, [req.body.name, req.body.image_url, req.user.id])
+      .then((results) => {
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500);
+      });
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 /**
  * Delete an item
  */
-router.delete('/:id', (req, res) => {
-  // endpoint functionality
+router.delete("/:itemId", (req, res) => {
+  const itemId = req.params.itemId;
+
+  if (req.isAuthenticated()) {
+    const userId = req.user.id;
+
+    let deleteQuery = `DELETE FROM "item" WHERE "id" = $1 AND "user_id" = $2;`;
+    pool
+      .query(deleteQuery, [itemId, userId])
+      .then((results) => {
+        res.sendStatus(204); // No content (successful deletion)
+      })
+      .catch((error) => {
+        console.log(error);
+        res.sendStatus(500); // internal server errr
+      });
+  } else {
+    res.sendStatus(401); // unautherized?
+  }
 });
 
 module.exports = router;
--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,46 +1,90 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './ShelfPage.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./ShelfPage.css";
 
 function ShelfPage() {
   const [shelfList, setShelfList] = useState([]);
+  const [itemName, setItemName] = useState('');
+  const [image_url, setImage_url] = useState('');
 
   useEffect(() => {
     fetchShelf();
   }, []);
 
   const fetchShelf = () => {
-    axios.get('/api/shelf').then((response) => {
-      setShelfList(response.data);
-    }).catch((error) => {
-      console.log(error);
-      alert('Something went wrong.');
-    });
-  }
+    axios
+      .get("/api/shelf")
+      .then((response) => {
+        setShelfList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Something went wrong.");
+      });
+  };
+
+  const addItem = (e) => {
+    e.preventDefault();
+    axios
+      .post("/api/shelf", { name: itemName, image_url: image_url })
+      .then((response) => fetchShelf())
+      .catch((error) => {
+        console.error(error);
+        alert("Something went wrong.");
+      });
+  };
+
+  const deleteItem = (itemId) => {
+    axios
+      .delete(`/api/shelf/${itemId}`) 
+      .then((response) => {
+        fetchShelf();
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("Something went wrong while deleting the item.");
+      });
+  };
 
   return (
     <div className="container">
+      <h2>Add an Item</h2>
+      <form onSubmit={addItem}>
+        Name:{" "}
+        <input
+          type="text"
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)}
+        />
+        <br />
+        Image url:{" "}
+        <input
+          type="text"
+          value={image_url}
+          onChange={(e) => setImage_url(e.target.value)}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+      <hr />
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
-      {
-        shelfList.length === 0 && (
-          <div>No items on the shelf</div>
-        )
-      }
-      {
-        shelfList.map(item => {
-          return <div className="responsive" key={item.id}>
-                    <div className="gallery">
-                        <img src={item.image_url} alt={item.description} />
-                        <br />
-                        <div className="desc">{item.description}</div>
-                        <div style={{textAlign: 'center', padding: '5px'}}>
-                          <button style={{cursor: 'pointer'}}>Delete</button>
-                        </div>
-                    </div>
-                 </div>
-        })
-      }
+      {shelfList.length === 0 && <div>No items on the shelf</div>}
+      {shelfList.map((item) => {
+        return (
+          <div className="responsive" key={item.id}>
+            <div className="gallery">
+              <img src={item.image_url} alt={item.description} />
+              <br />
+              <div className="desc">{item.description}</div>
+              <div className="username">{item.username}</div>
+              <div style={{ textAlign: "center", padding: "5px" }}>
+                <button onClick={() => deleteItem(item.id)} style={{ cursor: "pointer" }}>Delete</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
       <div className="clearfix"></div>
     </div>
   );
